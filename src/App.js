099620117fe25fe6d0deb126162dd845dac6aab2
Daniel B Kim <danielkim@Daniels-MacBook-Pro.local>
2021-03-26 08:42:22 -0400

    adds necessary child components + dummy data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+
+const orders = [
+  {
+    name: 'Daniel',
+    pizzaSize: 10,
+    toppings: ['extra cheese', 'meatball'],
+    specialInstructions: 'leave pizza at the door'
+  },
+  {
+    name: 'Charles',
+    pizzaSize: 12,
+    toppings: ['pepperoni'],
+    specialInstructions: 'tip is in the mailbox'
+  },
+  {
+    name: 'George',
+    pizzaSize: 10,
+    toppings: ['sausage'],
+    specialInstructions: 'drive safely'
+  },
+  {
+    name: 'Sarah',
+    pizzaSize: 10,
+    toppings: [],
+    specialInstructions: 'it\'s free if it takes longer than 30 minutes right?'
+  }
+];
 
 const App = () => {
+
+
   return (
     <>
       <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+      <p>Your favorite food delivered while coding</p>
+
     </>
   );
 };
